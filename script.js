5ee934b45fdea067b403fdc1e19f2607eea60ce6
nateedralin <nate.edralin@gmail.com>
2026-03-06 11:01:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,29 +18,21 @@ const notes = document.getElementById("notes").value.trim();
 message.textContent = "";
 message.style.color = "red";
 
-/* Required field validation */
-
 if(!name || !email || !destination || !startDate || !endDate){
-message.textContent = "Please fill all required fields.";
+message.textContent = "Please fill in all required fields.";
 return;
 }
-
-/* Date validation */
 
 if(new Date(startDate) >= new Date(endDate)){
-message.textContent = "Start date must be before end date.";
+message.textContent = "Your start date must be before end date.";
 return;
 }
-
-/* Travelers validation */
 
 if(travelers <= 0 || travelers > 20){
 message.textContent = "Number of travelers must be between 1 and 20.";
 return;
 }
-
-/* JSON data */
-
+  
 const data = {
 name,
 email,
